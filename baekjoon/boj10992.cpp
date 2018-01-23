dae3c8898470d9d7afd9410a623e6da335fdbe28
SeungJu <safe586@naver.com>
2018-01-23 23:20:47 +0900

    boj10992 별찍기 17

---
+++ baekjoon/boj10992.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+int n;
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n + i; j++) printf(i == n - 1 || j == n - 1 - i || j == n - 1 + i ? "*" : " ");
+		puts("");
+	}
+	return 0;
+}
